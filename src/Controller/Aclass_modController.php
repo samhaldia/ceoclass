3dee1e185d2dd378239d65da6f0109b979772e80
Sameer Kumar Choudhary <schoudhary@liquidhub.com>
2018-06-01 12:03:03 +0530

    Controller commit

---
+++ src/Controller/Aclass_modController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\aclass_mod\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class Aclass_modController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function thankYou() {
+    return new JsonResponse (array('status' => 1,'message' => 'OK!'));
+  }
+
+}
